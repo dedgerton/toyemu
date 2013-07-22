c81922b84a0a91e7237fbd43add37348b3a8928d
Dakota Edgerton <dedgerton97@gmail.com>
2013-07-21 23:03:23 -0700

    Fixed license notification in all files; toyemu is not a library and is licensed under GPL, not LGPL. Also updated copyright notice from 2012 to 2012-2013.

--- incrementOpcodes.c
+++ incrementOpcodes.c
@@ -1,22 +1,22 @@
 /**
  * toyemu - 6502 CPU emulator
- * Copyright (C) 2012 Dakota Edgerton
+ * Copyright (C) 2012-2013 Dakota Edgerton
  * mail: dedgerton97@gmail.com
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
+ * This file is part of toyemu.
  *
- * This library is distributed in the hope that it will be useful,
+ * toyemu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * toyemu is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with toyemu.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "incrementOpcodes.h"
